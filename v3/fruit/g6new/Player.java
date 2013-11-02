26b85db393fc50398071b98bab0b3225e734435a
Ashlesha Shirbhate <ashlesha.shirbhate@gmail.com>
2013-11-02 15:29:31 -0700

    New deliverable

---
+++ v3/fruit/g6new/Player.java
@@ -0,0 +1,202 @@
+package fruit.g6new;
+
+import java.util.Arrays;
+
+public class Player extends fruit.sim.Player {
+  //Book-keeping
+  public final int NFRUIT = 12;
+  int nplayers;
+  int bowlSize;
+  int[] mPreferences = new int[NFRUIT];
+  double[] mExpDistrib = new double[NFRUIT];
+  
+  //State & property keepers
+  boolean firstInvocation;
+  int timesCanPass;
+  int timesCanPassInitial;
+  int passNumber;
+  int stoppingThreshold;
+  double deviationThreshold;
+  
+  //Parameter tuning
+  boolean replaceInitialDistribution=false;
+  boolean useStopping=true;
+  boolean debug=true;
+  boolean useLogisticFunctionDeviationFraction=true;
+  boolean laterBowlMoreImp=true; //still not using this
+  boolean takeReallyGoodBowl=true;
+  
+  public void init(int nplayers, int[] pref) {
+	  this.nplayers=nplayers;
+	  mPreferences=pref;
+	  Arrays.fill(mExpDistrib, 1.0/NFRUIT);
+	  roundInit(0);
+  }
+  
+  public void roundInit(int round)
+  {
+	  if(debug)
+		  print("Custom Initializing...");
+	  firstInvocation=true;
+	  passNumber=1;
+	  timesCanPass=nplayers-1-getIndex();
+	  if(round==1)
+	  {
+		  timesCanPass=nplayers-timesCanPass-1;
+	  }
+	  timesCanPassInitial=timesCanPass;
+	  stoppingThreshold=(int)Math.floor(timesCanPass/Math.E);
+  }
+
+  public boolean pass(int[] bowl, int bowlId, int round,
+                      boolean canPick,
+                      boolean musTake) {
+	  
+   bowlSize=sumOfArray(bowl);
+	  
+   if (musTake || !canPick) {
+     modifyDistribution(bowl);
+     roundInit(1);
+     return true;
+   }
+   
+   double expectedScore=getExpectedScore();
+   double bowlScore=getBowlScore(bowl);
+   double deviation=getDeviation();
+   
+   
+   
+   double meanPlus;
+   if(useLogisticFunctionDeviationFraction) //Chop off using logistic function 
+   {
+	   meanPlus=calculateDeviationThreshold()*scaledSigmoid(timesCanPass*1.0/timesCanPassInitial);
+   }
+   else //Linearly chop off
+   {
+	   meanPlus=deviation*timesCanPass/(timesCanPassInitial-1);
+   }
+   
+   if(debug)
+   {
+	   print(String.format("timesCanPass %d timesCanPassInitial %d expected %f deviation %f deviationfraction %f", timesCanPass, timesCanPassInitial, expectedScore, deviation,calculateDeviationThreshold()));
+	   print(String.format("Old expected=%f // bowlscore=%f ", (expectedScore+deviation*timesCanPass/(timesCanPassInitial-1)), bowlScore));
+	   print(String.format("New expected=%f // bowlscore=%f ", (expectedScore+calculateDeviationThreshold()*scaledSigmoid(1.0*timesCanPass/timesCanPassInitial)), bowlScore));
+	   print(String.format("stoppingthreshold=%d // passnumber=%d ", stoppingThreshold, passNumber));
+   }
+   
+   boolean take = ((expectedScore+meanPlus) <= bowlScore || (takeReallyGoodBowl && bowlScore>=9*NFRUIT));
+   modifyDistribution(bowl);
+   
+   
+   if(useStopping && passNumber<=stoppingThreshold)
+   {
+	   take=false;
+   }
+   
+   if(!take)
+ 	  timesCanPass--;
+   else
+ 	  roundInit(1);
+   
+   passNumber++;
+   return take;
+  }
+
+  public double getDeviation()
+  {
+	  double deviation=0;
+	  
+	  for (int i = 0; i < NFRUIT; i++) 
+	  {
+		  deviation += (mExpDistrib[i] * mPreferences[i] * mPreferences[i]);
+	  }
+	  
+	  return Math.sqrt(deviation);
+  }
+  
+  private double getExpectedScore() {
+    double expected = 0;
+    
+    for (int i = 0; i < NFRUIT; i++) {
+      expected += (mExpDistrib[i] * mPreferences[i]);
+    }
+    expected*=bowlSize;
+    
+    return expected;
+  }
+  
+  /* Returns score of current bowl */
+  private int getBowlScore(int[] bowl) {
+    int score = 0;
+    for (int i = 0; i < NFRUIT; i++) {
+      score += bowl[i] * mPreferences[i];
+    }
+    return score;
+  }
+  
+  /**
+   * Average the newBowl with the current expected distribution
+   * @param newBowl
+   */
+  private void modifyDistribution(int[] newBowl) {
+	  if(firstInvocation)
+	  {
+		  firstInvocation=false;
+		  if(replaceInitialDistribution)
+		  {
+			  Arrays.fill(mExpDistrib, 0);
+		  }
+	  }
+	  
+	  for(int i=0;i<newBowl.length;i++)
+	  {
+		  mExpDistrib[i]=(mExpDistrib[i]*mExpDistrib.length*passNumber + newBowl[i]);
+		  mExpDistrib[i]/=1.0*(mExpDistrib.length*passNumber+sumOfArray(newBowl));
+	  }
+  }
+  
+  /*
+   * Returns sum of elements in a int array
+   */
+  private int sumOfArray(int[] arr)
+  {
+	  int sum=0;
+	  for(double a: arr)
+	  {
+		  sum+=a;
+	  }
+	  return sum;
+  }
+  
+  /*
+   * Returns sum of elements in a double array
+   */
+  private double sumOfArray(double[] arr)
+  {
+	  double sum=0;
+	  for(double a: arr)
+	  {
+		  sum+=a;
+	  }
+	  return sum;
+  }
+  
+  public void print(String s)
+  {
+	  System.out.println(s);
+  }
+  
+  public double calculateDeviationThreshold()
+  {
+	 return getDeviation() * scaledSigmoid( 1.0*timesCanPass/ (nplayers-1) );
+  }
+  
+  
+  /* *
+   * Sigmoid function that takes in value from 0 to 1, and outputs values from 0 to 1 
+   * */
+  public double scaledSigmoid(double x)
+	{
+		return 1/(1+Math.pow(Math.E,6-x*12));
+	}
+}
---
+++ v3/fruit/g8new/Player.java
@@ -0,0 +1,171 @@
+package fruit.g8new;
+
+import java.util.*;
+
+public class Player extends fruit.sim.Player
+{
+	int nplayer;
+	int[] pref;
+	int[] record;
+	int magic;
+	int position;
+	int magic_table[]={-1,0,0,1,1,2,2,2,3,3};
+    public void init(int nplayers, int[] pref) {
+    	this.nplayer=nplayers;
+    	this.pref=pref;
+    	this.position=this.getIndex();// position start from 0
+    	this.record = new int[2*nplayer];
+    	if(nplayers-position<=9)
+        	magic=magic_table[nplayers-position];
+        else
+        	magic=(int) Math.round(0.369*(nplayers-position) );
+    }
+    int max=0;
+    int counter=0;
+    int bowSize=0;
+    public boolean pass(int[] bowl, int bowlId, int round,
+                        boolean canPick,
+                        boolean musTake) {
+    	counter++;
+    	if(counter==1){
+    		for(int i=0;i<12;i++){
+        		bowSize+=bowl[i];
+        	}
+    		//update(bowSize*6);
+    	}
+    	//System.out.printf("\n counter is %d\n", counter);
+    	//record[counter-1]=score(bowl);
+    	update(score(bowl));
+    	
+    	if (musTake){
+			return true;
+		}
+    	
+    	//no enough information
+    	if (info.size()<=1) {
+			return false;
+		}
+    	
+    	int futureBowls=0;
+    	if (round==0) {
+    		futureBowls=nplayer-position-counter;
+            //return round0(bowl,bowlId,round,canPick,musTake);
+        } else {
+        	futureBowls=position+1-counter;
+            //return round1(bowl,bowlId,round,canPick,musTake);
+        }
+    	double b=score(bowl);
+    	double PrB=1, p=probLessThan(b);
+    	for (int i = 0; i < futureBowls; i++) {
+			PrB*=p;
+		}
+    	double PrA=1-PrB;
+    	double ExA=exptGreaterThan(b);
+    	double ExB=exptLessThan(b);
+    	double Ex2=PrA*ExA+PrB*ExB;
+    	if(Ex2>b) { //
+    		return false;
+    	}
+    	else {
+			return true;
+		}
+    }
+    private double exptLessThan(double b) {
+    	double sum=0,interval=(b-12)/10000,val=12;
+		for (int i = 0; i <10000; i++) {
+			sum+=phi(val, mu, sigma)*val*interval;
+			val+=interval;
+		}
+		return sum/probLessThan(b);
+	}
+	private double exptGreaterThan(double b) {
+		double sum=0,interval=(bowSize*12-b)/10000,val=b;
+		for (int i = 0; i <10000; i++) {
+			sum+=phi(val, mu, sigma)*val*interval;
+			val+=interval;
+		}
+		return sum/(1-probLessThan(b));
+	}
+	private double probLessThan(double b) {
+		if (sigma<1e-8) {
+			if (b>mu) {
+				return 1;
+			}else {
+				return 0;
+			}
+		}
+		return Phi((b - mu) / sigma);
+	}
+    
+    public static double Phi(double z) {
+        if (z < -8.0) return 0.0;
+        if (z >  8.0) return 1.0;
+        double sum = 0.0, term = z;
+        for (int i = 3; sum + term != sum; i += 2) {
+            sum  = sum + term;
+            term = term * z * z / i;
+        }
+        return 0.5 + sum * phi(z);
+    }
+    
+    // return phi(x) = standard Gaussian pdf
+    public static double phi(double x) {
+        return Math.exp(-x*x / 2) / Math.sqrt(2 * Math.PI);
+    }
+    public static double phi(double x, double mu, double sigma) {
+        return phi((x - mu) / sigma) / sigma;
+    }
+    
+	ArrayList<Integer> info=new ArrayList<Integer>();
+    double mu,sigma;
+	private void update(int x) {
+		info.add(x);
+		mu=0;
+		for(Integer y:info){
+			mu+=y;
+		}
+		mu=mu/info.size();
+		sigma=0;
+		for(Integer y:info){
+			sigma+=(y-mu)*(y-mu);
+		}
+		sigma/=info.size();
+		sigma=Math.sqrt(sigma);
+	}
+
+
+
+	private int score(int[] bowl){
+    	int sum=0;
+    	for(int i=0;i<12;i++){
+    		sum+=pref[i]*bowl[i];
+    	}
+    	return sum;
+    }
+
+	private boolean pickduringobservation(int[] bowl, int[] record) {
+		System.out.println("we are in the pickduring observation");
+		System.out.printf("\n the score for this bow is %d\n", score(bowl));
+		if (score(bowl)>average(record)*1.5 && score(bowl)>=max)
+			return true;
+		else
+			return false;
+	}
+	
+	private double average(int[] record){
+		int sum=0;
+		int i;
+		double avg;
+		int ct = 0;
+		for (i=0;i<record.length;i++){
+			sum = sum+record[i];
+			System.out.printf("\n the %d th record is %d\n",i, record[i]);
+			if (record[i]!=0)
+				ct=i;
+		}
+		avg = sum/(ct+1);
+		System.out.printf("\n the avg score is %f\n", avg);
+		return avg;
+	}
+    private Random random = new Random();
+}
